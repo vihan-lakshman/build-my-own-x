eb3e5c701b190bc44e5012ce254de9c3b0ca07e8
Vihan Lakshman <vihan@Vihans-MacBook-Pro.local>
2021-10-30 19:20:26 -0400

    First version of simple shell

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+/* A shell implementation in Rust */
+
+use std::process::Command;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    let stdin = io::stdin();
+    stdin.read_line(&mut input).unwrap();
+
+    // read_line leaves a trailing newline
+    let command = input.trim();
+
+    Command::new(command)
+        .spawn()
+        .unwrap();
 }
