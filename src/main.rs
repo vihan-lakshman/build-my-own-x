1c067433c17c5037f4c1259fedda0351232001dd
arichard <arichard@cs.stanford.edu>
2021-10-30 11:57:55 -0700

    Skip over filenames with dots

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,22 @@ use std::process::Command;
 use std::io;
 use std::fs;
 
+fn print_filename(file_name: String) {
+    if file_name.starts_with(".") {
+        return;
+    }
+    println!("{:?}", file_name);
+}
+
 fn ls() -> io::Result<()> {
     for entry in fs::read_dir(".")? {
         let entry = entry?;
         let file_name = entry.file_name();
-        // if file_name.into_string()?.starts_with(".") {
-        //     continue;
-        // }
-        println!("{:?}", file_name);
+        let str_file_name = file_name.into_string();
+        match str_file_name {
+            Ok(str_file_name) => print_filename(str_file_name),
+            Err(_str_file_name) => (), // invalid unicode name
+        }
     }
     Ok(())
 }
