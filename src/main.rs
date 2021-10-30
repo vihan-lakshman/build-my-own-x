654fc06ff51ed3248ffe829bd54361cb4eb2f976
arichard <arichard@cs.stanford.edu>
2021-10-30 11:50:59 -0700

    Implemented ls

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,19 @@
 
 use std::process::Command;
 use std::io;
+use std::fs;
+
+fn ls() -> io::Result<()> {
+    for entry in fs::read_dir(".")? {
+        let entry = entry?;
+        let file_name = entry.file_name();
+        // if file_name.into_string()?.starts_with(".") {
+        //     continue;
+        // }
+        println!("{:?}", file_name);
+    }
+    Ok(())
+}
 
 fn main() {
     let mut input = String::new();
@@ -11,7 +24,14 @@ fn main() {
     // read_line leaves a trailing newline
     let command = input.trim();
 
-    Command::new(command)
-        .spawn()
-        .unwrap();
+    if command == "ls" {
+        let output = ls();
+        if let Err(output) = output {
+            println!("{:?}", output);
+        }
+    } else {
+        Command::new(command)
+            .spawn()
+            .unwrap();
+    }
 }
