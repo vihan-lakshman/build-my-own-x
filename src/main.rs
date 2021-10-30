88d0a8d186bb9c40aeb377b662ec43a7e55cca17
arichard <arichard@cs.stanford.edu>
2021-10-30 12:47:44 -0700

    More colors: cyan for symlinks, green for executables

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,29 @@
 use std::process::Command;
 use std::io;
 use std::fs;
+use std::os::unix::fs::PermissionsExt;
 
 use colored::*;
 
-fn print_filename(file_name: String, is_folder: bool) {
+fn print_filename(file_name: String, metadata: fs::Metadata) {
     if file_name.starts_with(".") {
         return;
     }
-    if is_folder {
-        println!("{}", file_name.blue());
-    } else {
-        println!("{}", file_name);
+    if metadata.is_dir() {
+        print!("{}  ", file_name.blue());
+        return;
     }
+    if metadata.file_type().is_symlink() {
+        print!("{}  ", file_name.cyan());
+        return;
+    }
+    // Executable; will only work on POSIX; I don't care about Windows
+    let permissions = metadata.permissions();
+    if permissions.mode() & 0o111 != 0 {
+        print!("{}  ", file_name.green());
+        return;
+    }
+    print!("{}  ", file_name.white());
 }
 
 fn ls() -> io::Result<()> {
@@ -24,7 +35,7 @@ fn ls() -> io::Result<()> {
         let metadata = entry.metadata()?;
         let str_file_name = file_name.into_string();
         match str_file_name {
-            Ok(str_file_name) => print_filename(str_file_name, metadata.is_dir()),
+            Ok(str_file_name) => print_filename(str_file_name, metadata),
             Err(_str_file_name) => (), // invalid unicode name
         }
     }
